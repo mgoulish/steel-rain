7b9cc7b3ea5f45d9c07a7524a50525f9ea1ed30e
mgoulish <mgoulish@redhat.com>
2022-01-11 03:12:41 -0500

    Switched to global context dict. Router env paths are correct now.

--- scripts/steel-rain.py
+++ scripts/steel-rain.py
@@ -10,6 +10,10 @@ import socket
 
 
 
+context = {}
+
+
+
 def get_open_port():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("",0))
@@ -20,10 +24,15 @@ def get_open_port():
 
 
 def find_router ( ) :
-    return shutil.which ( 'qdrouterd' )
+    context['router'] = shutil.which ( 'qdrouterd' )
 
 
 def check_env ( ):
+    dispatch_install    = os.environ.get('STEEL_RAIN_DISPATCH_INSTALL')
+    if dispatch_install == None:
+      print ( "please set STEEL_RAIN_DISPATCH_INSTALL env var.")
+      sys.exit(1)
+
     proton_root    = os.environ.get('STEEL_RAIN_PROTON_ROOT')
     if proton_root == None:
       print ( "please set STEEL_RAIN_PROTON_ROOT env var.")
@@ -35,6 +44,12 @@ def check_env ( ):
       sys.exit(1)
 
 
+    # Make sure that the required directories actually exist.
+
+    if False == os.path.isdir ( dispatch_install ) :
+      print ( f"{dispatch_install} directory does not exist." )
+      sys.exit(1)
+
     if False == os.path.isdir ( proton_root ) :
       print ( f"{proton_root} directory does not exist." )
       sys.exit(1)
@@ -43,7 +58,9 @@ def check_env ( ):
       print ( f"{proton_install} directory does not exist." )
       sys.exit(1)
 
-    return proton_root, proton_install
+    context["dispatch_install"] = dispatch_install
+    context["proton_root"]      = proton_root
+    context["proton_install"]   = proton_install
 
 
 
@@ -59,14 +76,14 @@ def make_test_dir ( ) :
       sys.exit(1)
     os.mkdir ( test_dir )
     os.mkdir ( test_dir + "/config" )
-    return test_dir
+    context['test_dir'] = test_dir
    
     
-def make_router ( command, test_dir ) :
+def make_router ( command ) :
     name = command[0]
     threads = command[1]
     port = get_open_port()
-    config_file_name = test_dir + "/config/" + name + ".conf"
+    config_file_name = context['test_dir'] + "/config/" + name + ".conf"
     print ( f"config file: {config_file_name}" )
     f = open ( config_file_name, "w" )
     f.write("router {\n")
@@ -85,9 +102,16 @@ def make_router ( command, test_dir ) :
     f.write("}\n")
     f.close()
 
+    router_env = dict(os.environ)
+    router_env["LD_LIBRARY_PATH"] = context['dispatch_install'] + "/lib:" + context['proton_install'] + "/lib64"
+    router_env["PYTHONPATH"] = context['dispatch_install'] + "/lib/qpid-dispatch/python:" + context['dispatch_install'] + "/lib/python3.9/site-packages"
+
+    print ( f"LD_LIBRARY_PATH == |{router_env['LD_LIBRARY_PATH']}|\n" )
+    print ( f"PYTHONPATH == |{router_env['PYTHONPATH']}|\n" )
 
 
-def read_commands ( file_name, test_dir ) :
+
+def read_commands ( file_name ) :
     with open(file_name) as f:
         content = f.readlines()
     for line in content :
@@ -96,22 +120,25 @@ def read_commands ( file_name, test_dir ) :
         s = " "
         print(s.join(words[1:]))
       elif words[0] == 'router' :
-        make_router ( words[1:], test_dir )
+        make_router ( words[1:] )
 
 
 # Main program ----------------------------
 
-router = find_router()
-if router == None:
+find_router()
+if context['router'] == None:
   print ( "No qdrouterd in path." )
   sys.exit(1)
 
-proton_root, proton_install = check_env ( )
+check_env ( )
 
-print ( f"using proton_root:    {proton_root}" )
-print ( f"using proton_install: {proton_install}" )
-print ( f"using qdrouterd:      {router}" )
+print ( f"using dispatch_install: {context['dispatch_install']}" )
+print ( f"using proton_root:      {context['proton_root']}" )
+print ( f"using proton_install:   {context['proton_install']}" )
+print ( f"using qdrouterd:        {context['router']}" )
 
-test_dir = make_test_dir()
+make_test_dir ( )
 
-read_commands ( "./commands", test_dir )
+read_commands ( "./commands" )
+
+
